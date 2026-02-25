9cd67691f6870e17dc2c23c6b5d1a3eb433752e1
nolhan <nolhan.dev@gmail.com>
2026-02-25 15:50:36 +0100

    feat(composables): added useBotTeam composable

---
+++ src/composables/useBotTeam.ts
@@ -0,0 +1,38 @@
+import useAPI, { APIError, APIScope } from '@/utils/api'
+import { useStore } from '@/stores'
+import { computed } from 'vue'
+
+export default function useBotCustomEvents(botId: string) {
+  const api = useAPI(APIScope.User)
+  const store = useStore()
+
+  const team = computed(() => store.botTeams[botId] ?? [])
+
+  async function fetch() {
+    if (!api.userId) throw new APIError(401, 'Not authenticated')
+    store.botTeams[botId] = (await api.bots.getTeammates(botId)).team
+  }
+
+  async function add(
+    teammateId: string,
+    sendMethod?: 'mail',
+  ): Promise<{ invitationId: string; message: string }> {
+    if (!api.userId) throw new APIError(401, 'Not authenticated')
+    const result = await api.bots.addTeammate(botId, teammateId, sendMethod)
+    await fetch()
+    return result
+  }
+
+  async function remove(teammateId: string) {
+    if (!api.userId) throw new APIError(401, 'Not authenticated')
+    await api.bots.deleteTeammate(botId, teammateId)
+
+    const list = store.botTeams[botId]
+    if (!list) return
+
+    const eventIndex = list.findIndex((e) => e.userId === teammateId)
+    if (eventIndex >= 0) list.splice(eventIndex, 1)
+  }
+
+  return { team, fetch, remove }
+}
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,12 +1,13 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
-import type { Achievement, Bot, CustomEvent, User } from '@/utils/types.ts'
+import type { Achievement, Bot, CustomEvent, Teammate, User } from '@/utils/types.ts'
 
 export const useStore = defineStore('store', () => {
   const userInfos = ref<User | null>(null)
   const userBots = ref<Bot[]>([])
   const botAchievements = ref<{ [botId: string]: Achievement[] }>({})
   const botCustomEvents = ref<{ [botId: string]: CustomEvent[] }>({})
+  const botTeams = ref<{ [botId: string]: Teammate[] }>({})
 
-  return { userInfos, userBots, botAchievements, botCustomEvents }
+  return { userInfos, userBots, botAchievements, botCustomEvents, botTeams }
 })
