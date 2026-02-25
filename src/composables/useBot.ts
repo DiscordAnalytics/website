105bdb9c26bfb8e2090d69e8a8213c00b7480bbd
nolhan <nolhan.dev@gmail.com>
2026-02-25 14:17:08 +0100

    feat(composables): added useBot composable

---
+++ src/composables/useBot.ts
@@ -0,0 +1,32 @@
+import useAPI, { APIError, APIScope } from '@/utils/api'
+import { useStore } from '@/stores'
+import { computed } from 'vue'
+
+export default function useBot(botId: string, scope: APIScope = APIScope.User) {
+  const api = useAPI(scope)
+  const store = useStore()
+
+  const bot = computed(() => store.userBots.find((bot) => bot.botId === botId))
+
+  async function remove() {
+    await api.bots.deleteBot(botId)
+    store.userBots = store.userBots.filter((bot) => bot.botId !== botId)
+  }
+
+  async function regenToken() {
+    if (!bot.value) throw new APIError(404, 'Bot not found')
+    await api.bots.regenToken(botId, bot.value.token)
+  }
+
+  async function toggleAdvancedStats() {
+    if (!bot.value) throw new APIError(404, 'Bot not found')
+    await api.bots.updateSettings(botId, { advanced_stats: !bot.value.advancedStats })
+  }
+
+  async function updateVotesWebhook(webhookUrl: string) {
+    if (!bot.value) throw new APIError(404, 'Bot not found')
+    await api.bots.updateVotesWebhook(botId, webhookUrl)
+  }
+
+  return { bot }
+}
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -4,7 +4,7 @@ import type { Bot, User } from '@/utils/types.ts'
 
 export const useStore = defineStore('store', () => {
   const userInfos = ref<User | null>(null)
-  const userBots = ref<Bot[] | null>(null)
+  const userBots = ref<Bot[]>([])
 
   return { userInfos, userBots }
 })
