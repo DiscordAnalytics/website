525115bfd2f2a4aeab8eabe4f4d333cfc005884b
nolhan <nolhan.dev@gmail.com>
2026-02-25 16:32:18 +0100

    fix(composables): added useBotStats composable

---
+++ src/composables/useBotStats.ts
@@ -0,0 +1,17 @@
+import useAPI, { APIScope } from '@/utils/api'
+import { useStore } from '@/stores'
+import { computed } from 'vue'
+import type { DateRange } from 'reka-ui'
+
+export function useBotStats(botId: string) {
+  const api = useAPI(APIScope.User)
+  const store = useStore()
+
+  const stats = computed(() => store.botStats[botId] ?? null)
+
+  async function fetch(range: DateRange) {
+    store.botStats[botId] = await api.bots.getStats(botId, range)
+  }
+
+  return { stats, fetch }
+}
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,6 +1,15 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
-import type { Achievement, Bot, CustomEvent, StatsReport, Teammate, User } from '@/utils/types.ts'
+import type {
+  Achievement,
+  Bot,
+  CustomEvent,
+  RawStats,
+  RawVotes,
+  StatsReport,
+  Teammate,
+  User,
+} from '@/utils/types.ts'
 
 export const useStore = defineStore('store', () => {
   const userInfos = ref<User | null>(null)
@@ -9,6 +18,15 @@ export const useStore = defineStore('store', () => {
   const botCustomEvents = ref<{ [botId: string]: CustomEvent[] }>({})
   const botTeams = ref<{ [botId: string]: Teammate[] }>({})
   const botEmailReports = ref<{ [botId: string]: StatsReport[] }>({})
+  const botStats = ref<{ [botId: string]: { stats: RawStats[]; votes: RawVotes[] } }>({})
 
-  return { userInfos, userBots, botAchievements, botCustomEvents, botTeams, botEmailReports }
+  return {
+    userInfos,
+    userBots,
+    botAchievements,
+    botCustomEvents,
+    botTeams,
+    botEmailReports,
+    botStats,
+  }
 })
