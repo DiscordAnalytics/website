071aa379011fe74e550232fe5d569a9ac50e7e9f
nolhan <nolhan.dev@gmail.com>
2026-03-01 11:23:30 +0100

    feat(app): added vue-i18n

---
+++ src/locales/index.ts
@@ -0,0 +1,19 @@
+// English
+import * as EnHome from './en/pages/home.json'
+
+// French
+import * as FrHome from './fr/pages/home.json'
+
+export const maintainedLocales = ['en', 'fr']
+
+export const english = {
+  pages: {
+    home: EnHome,
+  },
+}
+
+export const french = {
+  pages: {
+    home: FrHome,
+  },
+}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
-import '@/assets/index.css'
-
 import App from './App.vue'
 import router from './router'
+import '@/assets/index.css'
+import { createI18n } from 'vue-i18n'
+import * as locales from './locales'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+type MessageFormat = typeof locales.english
+export const i18n = createI18n<[MessageFormat], 'en' | 'fr'>({
+  legacy: false,
+  globalInjection: true,
+  locale: 'en',
+  messages: {
+    en: locales.english,
+    fr: locales.french,
+  },
+})
+
+app.use(createPinia()).use(router).use(i18n)
 
 app.mount('#app')
