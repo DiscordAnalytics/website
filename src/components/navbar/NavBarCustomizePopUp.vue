d84e279238f44512c41e694031af9f52745dcbc4
nolhan <nolhan.dev@gmail.com>
2026-03-05 19:22:27 +0100

    refactor(navbar): enhanced theme selector

--- src/components/navbar/NavBarCustomizePopUp.vue
+++ src/components/navbar/NavBarCustomizePopUp.vue
@@ -1,42 +1,46 @@
 <script setup lang="ts">
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { Button } from '@/components/ui/button'
-import { Separator } from '@/components/ui/separator'
 import { MonitorIcon, MoonIcon, PaletteIcon, SunIcon } from 'lucide-vue-next'
 import { useStore } from '@/stores'
 import { ref } from 'vue'
 import { useColorMode, useLocalStorage } from '@vueuse/core'
 import { useI18n } from 'vue-i18n'
 import { toast } from 'vue-sonner'
-import { isChristmas } from '@/components/navbar/index.ts'
-import { Switch } from '@/components/ui/switch'
+import type { Color } from '@/utils/types.ts'
 import { cn } from '@/lib/utils.ts'
 import { ButtonGroup } from '@/components/ui/button-group'
+import { storeToRefs } from 'pinia'
+import { Switch } from '@/components/ui/switch'
+import { isChristmas } from '@/components/navbar/index.ts'
 
-const { setTheme } = useStore()
+const store = useStore()
+const { theme: currentTheme } = storeToRefs(store)
 const i18n = useI18n()
-const mode = useColorMode()
+const mode = useColorMode({ emitAuto: true })
 
 const carrotClick = ref<number>(0)
 const isCarroted = useLocalStorage('isCarroted', 'false')
 const isSnowEnabled = useLocalStorage('snowEnabled', 'true')
 
-const colorSelectorClasses = 'rounded-full h-4 w-4 cursor-pointer'
+const themes = [
+  { name: 'zinc', class: 'bg-zinc-600' },
+  { name: 'orange', class: 'bg-orange-600' },
+  { name: 'red', class: 'bg-red-600' },
+  { name: 'blue', class: 'bg-blue-600' },
+  { name: 'green', class: 'bg-green-600' },
+] as { name: Color; class: string }[]
 
 function handleCarrotClick() {
-  setTheme('orange')
+  store.setTheme('orange')
   ++carrotClick.value
   if (carrotClick.value > 3) {
     if (isCarroted.value === 'false') {
       isCarroted.value = 'true'
-      toast({
-        title: i18n.t('components.navbar.carroted'),
-      })
+      toast(i18n.t('components.navbar.carroted'))
       carrotClick.value = 0
     } else {
-      toast({
-        title: i18n.t('components.navbar.uncarroted'),
-      })
+      toast(i18n.t('components.navbar.uncarroted'))
       isCarroted.value = 'false'
       carrotClick.value = 0
     }
@@ -52,92 +56,73 @@ function handleCarrotClick() {
       </Button>
     </PopoverTrigger>
     <PopoverContent>
-      <h1 class="text-2xl font-bold my-4">
-        {{ $t('components.navbar.customize.title') }}
-      </h1>
-      <Separator />
-      <h2 class="text-lg font-semibold mt-2">
-        {{ $t('components.navbar.customize.color') }}
-      </h2>
-      <div class="flex items-center gap-4 p-4">
-        <span
-          :class="
-            cn(colorSelectorClasses, 'theme-zinc:border-2 theme-zinc:border-foreground bg-zinc-600')
-          "
-          @click="setTheme('zinc')"
-        />
-        <span
-          :class="
-            cn(
-              colorSelectorClasses,
-              'theme-orange:border-2 theme-orange:border-foreground bg-orange-600',
-            )
-          "
-          @click="handleCarrotClick"
-        />
-        <span
-          :class="
-            cn(colorSelectorClasses, 'theme-red:border-2 theme-red:border-foreground bg-red-600')
-          "
-          @click="setTheme('red')"
-        />
-        <span
-          :class="
-            cn(colorSelectorClasses, 'theme-blue:border-2 theme-blue:border-foreground bg-blue-600')
-          "
-          @click="setTheme('blue')"
-        />
-        <span
-          :class="
-            cn(
-              colorSelectorClasses,
-              'theme-green:border-2 theme-green:border-foreground bg-green-600',
-            )
-          "
-          @click="setTheme('green')"
-        />
-      </div>
-      <Separator />
-      <h2 class="text-lg font-semibold mt-2">
-        {{ $t('components.navbar.customize.theme.title') }}
-      </h2>
-      <ButtonGroup>
-        <Button
-          variant="outline"
-          size="sm"
-          :class="{ 'bg-accent text-accent-foreground': mode === 'light' }"
-          @click="mode = 'light'"
-        >
-          <SunIcon class="mr-2 h-4 w-4" />
-          Light
-        </Button>
+      <div class="grid gap-4">
+        <div class="space-y-2">
+          <h4 class="font-medium leading-none">
+            {{ $t('components.navbar.customize.title') }}
+          </h4>
+          <p class="text-sm text-muted-foreground">
+            {{ $t('components.navbar.customize.description') }}
+          </p>
+        </div>
+        <div class="grid gap-2">
+          <div class="flex justify-between items-center">
+            <p class="text-sm w-fit">{{ $t('components.navbar.customize.color') }}</p>
+            <ButtonGroup>
+              <Button
+                v-for="t in themes"
+                :key="t.name"
+                variant="outline"
+                size="icon"
+                :class="cn('h-9 flex-1 p-0', currentTheme === t.name && 'bg-accent')"
+                @click="t.name === 'orange' ? handleCarrotClick() : store.setTheme(t.name)"
+              >
+                <span :class="cn('h-4 w-4 rounded-full', t.class)" aria-hidden="true" />
+              </Button>
+            </ButtonGroup>
+          </div>
+          <div class="flex justify-between items-center">
+            <p class="text-sm w-fit">{{ $t('components.navbar.customize.theme.title') }}</p>
+            <ButtonGroup>
+              <Button
+                variant="outline"
+                size="icon"
+                :class="{ 'bg-accent text-accent-foreground': mode === 'light' }"
+                class="flex-1 p-0"
+                @click="mode = 'light'"
+              >
+                <SunIcon class="h-4 w-4" />
+              </Button>
 
-        <Button
-          variant="outline"
-          size="sm"
-          :class="{ 'bg-accent text-accent-foreground': mode === 'dark' }"
-          @click="mode = 'dark'"
-        >
-          <MoonIcon class="mr-2 h-4 w-4" />
-          Dark
-        </Button>
+              <Button
+                variant="outline"
+                size="icon"
+                :class="{ 'bg-accent text-accent-foreground': mode === 'dark' }"
+                class="flex-1 p-0"
+                @click="mode = 'dark'"
+              >
+                <MoonIcon class="h-4 w-4" />
+              </Button>
 
-        <Button
-          variant="outline"
-          size="sm"
-          :class="{ 'bg-accent text-accent-foreground': mode === 'auto' }"
-          @click="mode = 'auto'"
-        >
-          <MonitorIcon class="mr-2 h-4 w-4" />
-          Auto
-        </Button>
-      </ButtonGroup>
-      <div v-if="isChristmas" class="flex items-center justify-between gap-2">
-        {{ $t('components.navbar.customize.enableSnow') }}
-        <Switch
-          :default-value="isSnowEnabled === 'true'"
-          @update:model-value="(value: boolean) => (isSnowEnabled = String(value))"
-        />
+              <Button
+                variant="outline"
+                size="sm"
+                :class="{ 'bg-accent text-accent-foreground': mode === 'auto' }"
+                class="h-9"
+                @click="mode = 'auto'"
+              >
+                <MonitorIcon class="h-4 w-4" />{{ $t('components.navbar.customize.theme.system') }}
+              </Button>
+            </ButtonGroup>
+          </div>
+          <div v-if="isChristmas" class="flex justify-between items-center">
+            <p class="text-sm w-fit">{{ $t('components.navbar.customize.enableSnow') }}</p>
+            <Switch
+              :default-value="isSnowEnabled === 'true'"
+              @update:model-value="(value: boolean) => (isSnowEnabled = String(value))"
+            />
+          </div>
+        </div>
       </div>
     </PopoverContent>
   </Popover>
