da44ad9fe203d5b874419554d61ae3233e28e0a0
nolhan <nolhan.dev@gmail.com>
2026-03-07 12:01:39 +0100

    chore(eslint): added @intlify/eslint-plugin-vue-i18n to eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,6 +4,7 @@ import pluginVue from 'eslint-plugin-vue'
 import pluginOxlint from 'eslint-plugin-oxlint'
 import skipFormatting from 'eslint-config-prettier/flat'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import vueI18n from '@intlify/eslint-plugin-vue-i18n'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -27,9 +28,23 @@ export default defineConfigWithVueTs(
 
   eslintPluginPrettierRecommended,
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  ...vueI18n.configs.recommended,
+
   {
     rules: {
       'vue/multi-word-component-names': 'off',
+      '@intlify/vue-i18n/no-raw-text': 'error',
+      '@intlify/vue-i18n/no-missing-keys': 'off',
+      '@intlify/vue-i18n/no-unused-keys': 'off',
+      '@intlify/vue-i18n/key-format-style': ['error', 'camelCase'],
+    },
+    settings: {
+      'vue-i18n': {
+        localeDir: './src/locales/{en,fr}/**/*.json',
+        messageSyntaxVersion: '^11.0.0',
+      },
     },
   },
 )
