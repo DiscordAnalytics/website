1561a28156c1293c4398c89d738af9f74cfb8325
nolhan <nolhan.dev@gmail.com>
2026-03-08 10:20:34 +0100

    fix(layouts): fixed PageLayout when footer is disabled

--- src/components/layouts/PageLayout.vue
+++ src/components/layouts/PageLayout.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { NavBar } from '@/components/navbar'
 import Footer from '@/components/Footer.vue'
+import { cn } from '@/lib/utils.ts'
 
 withDefaults(
   defineProps<{
@@ -15,7 +16,14 @@ withDefaults(
 </script>
 
 <template>
-  <main class="max-w-425 mx-auto px-4 min-h-screen flex flex-col justify-between">
+  <main
+    :class="
+      cn(
+        'max-w-425 mx-auto px-4 min-h-screen flex flex-col',
+        $props.footer ? 'justify-between' : '',
+      )
+    "
+  >
     <NavBar v-if="$props.navbar" />
 
     <slot />
