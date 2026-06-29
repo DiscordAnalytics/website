3a8053592d4738d9cd9b89041a6c22b1996e5a29
Nolhan <nolhan.dev@gmail.com>
2026-06-29 10:49:51 +0200

    chore: removed unused import in scrollTo.ts

--- src/utils/scrollTo.ts
+++ src/utils/scrollTo.ts
@@ -1,5 +1,3 @@
-import { useRouter } from 'vue-router'
-
 export default function scrollTo(id: string) {
   const element = document.getElementById(id) as HTMLElement | undefined
   if (!element) return
